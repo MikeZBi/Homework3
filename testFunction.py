5d0fac2a2f3a50307e7c7e3b2597fef20a221c0e
MikeZBi <mikezengbi@gmail.com>
2018-11-01 15:14:19 -0700

    Update testFunction.py

--- testFunction.py
+++ testFunction.py
@@ -1,7 +1,5 @@
 import math
 
-
-
 ##########################
 #
 # Summary: Functions works the same as before, but to accomodate for no mutation we used list slicing.
@@ -24,8 +22,8 @@ def recNodeBreak(theString, nodeSize, numberInNode, whichNode, listOfLists, i):
         #print(combinedList)
         return combinedList
     elif (i == -1):
-        print(listOfLists)
-        print(type(listOfLists))
+        #print(listOfLists)
+        #print(type(listOfLists))
         return listOfLists
     elif (i > -1 and numberInNode < nodeSize):
         #listOfLists[whichNode].insert(0, (int(theString[i])))
@@ -48,6 +46,73 @@ def recNodeBreak(theString, nodeSize, numberInNode, whichNode, listOfLists, i):
         #print(combinedList)
         return(recNodeBreak(theString, nodeSize, 1 ,whichNode -1, combinedList, i - 1))
 
+
+####################################################################################
+#  Summary: Similar as before, but no mutation of the orginal passed in list, used list slicing and
+#           and concatenation of lists to achieve this instead of insertions
+#
+#
+#
+####################################################################################
+
+
+def zeroPad(whichNode, nodeSize, listA, listB):
+    if (len(listA) == len(listB) and whichNode == len(listA)):
+        # print("if")
+        return listA
+
+    elif (len(listA) != len(listB)):
+        # print("elif1")
+        if (len(listA) < len(listB)):
+            #listA.insert(0, [])
+            leftList = [[]]
+            rightList = listA
+            combined = leftList + rightList
+            return(zeroPad(whichNode, nodeSize, combined, listB))
+        else:
+            #listB.insert(0, [])
+            leftList = [[]]
+            rightList = listB
+            combined = leftList + rightList
+            return(zeroPad(whichNode, nodeSize, listA, combined))
+
+    elif (len(listA[whichNode]) < nodeSize or len(listB[whichNode]) < nodeSize):
+        # print("elif2", whichNode)
+        # print(listA)
+        # print(listB)
+
+        if (len(listA[whichNode]) < nodeSize):
+            # print("elif2 if")
+
+            #listA[whichNode].insert(0, 0)
+            leftList = listA[:whichNode]
+            middleList = [[0] + listA[whichNode]]
+            rightList = listA[whichNode + 1:]
+
+            combinedList = leftList + middleList + rightList
+
+            # print(listA)
+            return(zeroPad(whichNode, nodeSize, combinedList, listB))
+        else:
+            # print("elif2 else")
+
+            #listB[whichNode].insert(0, 0)
+            leftList = listB[:whichNode]
+            middleList = [[0] + listB[whichNode]]
+            rightList = listB[whichNode + 1:]
+
+            combinedList = leftList + middleList + rightList
+
+            # print(listB)
+            return(zeroPad(whichNode, nodeSize, listA, combinedList))
+
+    elif (len(listA[whichNode]) == nodeSize and len(listB[whichNode]) == nodeSize):
+        # print("elif3")
+
+        #whichNode += 1
+        return(zeroPad(whichNode+1, nodeSize, listA, listB))
+
+
 myString = "123456789"
 nodeSize = 3
 numberInNode = 0
@@ -59,12 +124,34 @@ for i in range(numberOfNodes):
     listOfLists[i] = []
 i = len(myString) - 1
 
+c = recNodeBreak(myString, nodeSize, numberInNode, whichNode, listOfLists, i)
+print(c)
 
+
+myString1 = "1234"
+nodeSize1 = 3
+numberInNode1 = 0
+whichNode1 = (math.ceil(len(myString1) / nodeSize1)) - 1
+# Initialize a 2dList, create another variable called numberOfNodes for ease of use
+numberOfNodes1 = (math.ceil(len(myString1) / nodeSize1))
+listOfLists1 = [0] * (numberOfNodes1)
+for i in range(numberOfNodes1):
+    listOfLists1[i] = []
+j = len(myString1) - 1
 
 
 # this is the recursion driver method, uncommet the prints in the method to show the process
-c =recNodeBreak(myString, nodeSize, numberInNode, whichNode, listOfLists, i)
-print(c)
+d = recNodeBreak(myString1, nodeSize1, numberInNode1, whichNode1, listOfLists1, j)
+print(d)
+
+
+thisNode = 0
+h = zeroPad(thisNode, nodeSize, c, d)
+z = zeroPad(thisNode, nodeSize, d, c)
+
+print(h)
+print(z)
+
 
 
 # #print(listOfLists)
